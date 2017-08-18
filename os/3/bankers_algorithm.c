4776b9565582deda3ec0147774a80e4bd005b236
santhisenan <santhisenan@outlook.com>
2017-08-18 06:38:43 +0530

    bankers algorithm

--- os/3/bankers_algorithm.c
+++ os/3/bankers_algorithm.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char const *argv[]) {
+int isSafe();
+int requestResource();
 
+int available[3] , max[5][3] , allocation[5][3] , need[5][3];
+
+int main(int argc, char const *argv[]) {
+  int safe;;
   //clear the console
   system("clear");
   printf("Assume that the total number of resources are 3 , number of processes are 5\n" );
 
-  int work[3] , available[3] , max[5][3] , allocation[5][3] , need[5][3] , request[3];
-  int finish[5];
-  int safe = 1;
   printf("Enter available resources\n");
   for (int i = 0; i < 3; i++) {
-    printf("Enter the total number of available resources of type %d\n", i );
+    printf("Enter the total number of available resources of type %d\n\n", i );
     scanf("%d", &available[i]);
   }
 
   printf("\nEnter the number of resources of each type currently allocated to processes\n");
   for (int i = 0; i < 5; i++) {
-    printf("Enter the number of allocated resources for process %d\n\t", i );
+    printf("Enter the number of allocated resources for process %d\n\n", i );
     for (int j = 0; j < 3; j++) {
       printf("Enter the number of allocated resources of type %d\n", j );
       scanf("%d", &allocation[i][j]);
@@ -28,7 +30,7 @@ int main(int argc, char const *argv[]) {
 
   printf("\nEnter the maximum demand of resources of each type for each process\n");
   for (int i = 0; i < 5; i++) {
-    printf("Enter the maximum demand for process %d\n\t", i );
+    printf("Enter the maximum demand for process %d\n\n", i );
     for (int j = 0; j < 3; j++) {
       printf("Enter the max demand of resources of type %d\n", j );
       scanf("%d", &max[i][j]);
@@ -41,6 +43,27 @@ int main(int argc, char const *argv[]) {
     }
   }
 
+  //call isSafe function
+  safe = isSafe();
+  if(safe)
+    printf("The system is safe!\n");
+  else
+    printf("The system is not safe\n");
+
+    //call resource request function
+    printf("Resource request algorithm\n");
+    safe = requestResource();
+    if(safe)
+      printf("The system will be safe after allocaitng the resources!\n");
+    else
+      printf("The system will not be safe after allocaitng the resources!\n");
+
+return 0;
+}
+
+int isSafe(){
+  int safe = 1;
+  int work[3] , finish[5];
   //Safety Algorothm
 
   //intialising work = available
@@ -69,50 +92,54 @@ int main(int argc, char const *argv[]) {
   }
 
   if (safe == 1) {
-    printf("The system is in safe state.\n");
+    // printf("The system is in safe state.\n");
+    return 1;
   }
   else{
-    printf("The system is not in safe state\n");
-  }
-
-  //Resource request algorithm
-  int processNumber;
-
-  printf("Enter the process number (0-4)\n");
-  scanf("%d\n", &processNumber);
-
-  int hasExceededMax = 0;
-  printf("Enter the number of instances of each resource to be requested\n");
-  for (int i = 0; i < 3; i++) {
-    printf("Number of instances of resource %d\n");
-    scanf("%d\n", &request[i]);
-    if(request[i] > need[processNumber][i]){
-      printf("Exceeded the maximum number of resource instances that can be requested \n");
-      hasExceededMax = 1;
-      break;
-    }
-  }
-
-  if(!hasExceededMax){
-  int hasToWait = 0; //If the request is less than available the process must wait
-  for (int i = 0; i < 3; i++) {
-    if(request[i] < available[processNumber][i]){
-      hasToWait = 1;
-    }
-  }
-  if(!hasToWait){
-    int isNotAvailable = 0;
-    for (int i = 0; i < 3; i++) {
-      available[processNumber][i] = available[processNumber][i] - request[i];
-      allocation[processNumber][i] = allocation[processNumber][i] + request[i];
-      need[processNumber][i] = need[processNumber][i] - request[i];
-      }
-    }
+    // printf("The system is not in safe state\n");
+    return 0;
   }
 
 }
 
+int requestResource(){
 
+    //Resource request algorithm
+    int processNumber;
+    int request[3];
+    printf("Enter the process number (0-4)\n");
+    scanf("%d", &processNumber);
 
-  return 0;
+    int hasExceededMax = 0;
+    printf("Enter the number of instances of each resource to be requested\n");
+    for (int i = 0; i < 3; i++) {
+      printf("Number of instances of resource %d\n" , i);
+      scanf("%d", &request[i]);
+      if(request[i] > need[processNumber][i]){
+        printf("Exceeded the maximum number of resource instances that can be requested \n");
+        hasExceededMax = 1;
+        break;
+      }
+    }
+
+    if(!hasExceededMax){
+      int hasToWait = 0; //If the request is less than available the process must wait
+      for (int i = 0; i < 3; i++) {
+        if(request[i] < available[i]){
+          hasToWait = 1;
+        }
+      }
+      if(!hasToWait){
+        // int isNotAvailable = 0;
+        for (int i = 0; i < 3; i++) {
+          available[i] = available[i] - request[i];
+          allocation[processNumber][i] = allocation[processNumber][i] + request[i];
+          need[processNumber][i] = need[processNumber][i] - request[i];
+        }
+        if(isSafe())
+          return 1;
+        else
+          return 0;
+      }
+    }
 }
